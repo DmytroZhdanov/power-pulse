bd2e5cc51317a3fbbf9a1a5d02598698baab6727
DmytroZhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2023-10-20 21:41:48 +0300

    Fix Loader export

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,16 +1,18 @@
 import { ThreeDots } from 'react-loader-spinner';
 import { WrapThreeDots } from './Loader.styled';
 
-export const Loader = () => (
-  <WrapThreeDots>
-    <ThreeDots
-      height="90"
-      width="90"
-      radius="9"
-      color="#E6533C"
-      ariaLabel="three-dots-loading"
-      visible={true}
-      timeout={2000}
-    />
-  </WrapThreeDots>
-);
+export default function Loader() {
+  return (
+    <WrapThreeDots>
+      <ThreeDots
+        height="90"
+        width="90"
+        radius="9"
+        color="#E6533C"
+        ariaLabel="three-dots-loading"
+        visible={true}
+        timeout={2000}
+      />
+    </WrapThreeDots>
+  );
+}
