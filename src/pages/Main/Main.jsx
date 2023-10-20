4a1a8c8096c4f113868d1c412453a20ea903bc37
DmytroZhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2023-10-20 17:46:48 +0300

    Add murkup and styles for Main page

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,19 +1,35 @@
 import { Suspense } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useResolvedPath } from 'react-router-dom';
 
 import Loader from 'components/Loader/Loader';
 import Header from 'components/main/Header/Header';
 import StatisticsInfo from 'components/main/StatisticsInfo/StatisticsInfo';
+import { Container } from './Main.styled';
+
+import { ROUTER } from '../../utils/constants';
 
 export default function Main() {
+  const { pathname } = useResolvedPath();
+  const page = pathname.split('/')[1];
+  const showStatisticInfo =
+    page === ROUTER.WELCOME ||
+    page === ROUTER.SIGN_UP ||
+    page === ROUTER.SIGN_IN ||
+    page === ROUTER.DATA.split('/')[0];
+
   return (
     <>
-      <div>Main page</div>
       <Header />
-      <StatisticsInfo />
-      <Suspense fallback={<Loader />}>
-        <Outlet />
-      </Suspense>
+
+      <main>
+        <Container>
+          <Suspense fallback={<Loader />}>
+            <Outlet />
+          </Suspense>
+
+          {showStatisticInfo && <StatisticsInfo />}
+        </Container>
+      </main>
     </>
   );
 }
