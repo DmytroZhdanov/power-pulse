3fcc5cda8a30ba8243029107bc0b750cc7700b92
Sergey4857 <yasak.sergey@gmail.com>
2023-10-21 12:39:11 +0300

    UserBar

--- src/components/main/UserBar/UserBar.jsx
+++ src/components/main/UserBar/UserBar.jsx
@@ -1,3 +1,22 @@
+import {
+  SettingsLink,
+  SvgSettings,
+  Avatar,
+  SvgDefault,
+} from './UserBar.styled';
+import sprite from '../../../assets/images/sprite/sprite.svg';
+
 export default function UserBar() {
-  return <div>UserBar component</div>;
+  return (
+    <SettingsLink to="/profile">
+      <SvgSettings>
+        <use href={`${sprite}#settings`}></use>
+      </SvgSettings>
+      <Avatar>
+        <SvgDefault>
+          <use href={`${sprite}#user`}></use>
+        </SvgDefault>
+      </Avatar>
+    </SettingsLink>
+  );
 }
