1db62cf5a3d4ea29bf31de9ddd53dd5081632056
Avtark <103372792+VolontyrPavel@users.noreply.github.com>
2023-10-22 20:39:01 +0300

    Update SignUpForm.jsx

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { useFormik, FormikProvider, Form, useField } from 'formik';
 import {
   Text,
@@ -90,7 +91,7 @@ export default function SignUpForm() {
         <Button type="submit">Sign Up</Button>
         <Sign>
           <p>Already have account?</p>
-          <a href="">Sign In</a>
+          <Link to="/signin">Sign In</Link>
         </Sign>
       </Form>
     </FormikProvider>
