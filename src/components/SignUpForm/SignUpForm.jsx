4af5ace0b9cee78038ed73a0b8f67cb8c565ff9d
Avtark <103372792+VolontyrPavel@users.noreply.github.com>
2023-10-22 19:51:30 +0300

    Update SignUpForm.jsx

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -52,25 +52,16 @@ export default function SignUpForm() {
     validationSchema: Yup.object({
       name: Yup.string()
         .min(3)
-        .matches(
-          '^[а-яА-ЯёЁa-zA-Z][а-яА-ЯёЁa-zA-Z0-9-_.]$',
-          'The name must start with a letter',
-        )
+        .matches('^^[а-яА-ЯёЁa-zA-Z0-9]+$', 'The name must start with a letter')
         .required('This is a required field'),
       email: Yup.string()
         .email('Invalid email')
         .matches(
-          '^([a-z0-9_-]+.)*[a-z0-9_-]+@[a-z0-9_-]+(.[a-z0-9_-]+)*.[a-z]{2,6}$',
+          '^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$',
           'Enter valid values',
         )
         .required('This is a required field'),
-      password: Yup.string()
-        .min(6)
-        .matches(
-          '^(?=.*d)(?=.*[a-z])(?=.*[A-Z])(?!.*s).*$',
-          'Enter valid values',
-        )
-        .required('This is a required field'),
+      password: Yup.string().min(6).required('This is a required field'),
     }),
   });
 
