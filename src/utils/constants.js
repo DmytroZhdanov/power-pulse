a6f4343362c28ac2023e73f47f6b99592ac78dfd
DmytroZhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2023-10-22 18:27:27 +0300

    Amend routing constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,7 +6,7 @@ export const ROUTER = {
   PROFILE: 'profile',
   DIARY: 'diary',
   PRODUCTS: 'products',
-  EXERCISES: 'exercises/:category',
+  EXERCISES: 'exercises',
   SUBCATEGORY: ':subcategory',
 };
 
