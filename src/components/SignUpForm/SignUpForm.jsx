8c9fc42377c32d9707c21b74a7f71dd253922786
Avtark <103372792+VolontyrPavel@users.noreply.github.com>
2023-10-22 20:34:56 +0300

    Update SignUpForm.jsx

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -57,11 +57,17 @@ export default function SignUpForm() {
       email: Yup.string()
         .email('Invalid email')
         .matches(
-          '^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$',
+          '^([a-z0-9_-]+.)*[a-z0-9_-]+@[a-z0-9_-]+(.[a-z0-9_-]+)*.[a-z]{2,6}$',
           'Enter valid values',
         )
         .required('This is a required field'),
-      password: Yup.string().min(6).required('This is a required field'),
+      password: Yup.string()
+        .min(6)
+        .matches(
+          '^(?=.*d)(?=.*[a-zA-Z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,32}$',
+          'Enter valid values',
+        )
+        .required('This is a required field'),
     }),
   });
 
