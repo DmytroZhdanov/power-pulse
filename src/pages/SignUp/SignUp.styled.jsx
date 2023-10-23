c7629bb5fec826d1e124c9dc51e976aa3aa87270
DmytroZhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2023-10-23 20:49:27 +0300

    Add container to SignUp page

---
+++ src/pages/SignUp/SignUp.styled.jsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  padding-top: 127px;
+
+  @media screen and (min-width: 768px) {
+    padding-top: 189px;
+  }
+  @media screen and (min-width: 1440px) {
+    padding-top: 200px;
+  }
+`;
