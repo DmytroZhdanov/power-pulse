2bb0ba7742460f26b40806878ac0a694dada3d5b
DmytroZhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2023-10-24 11:23:00 +0300

    Add Private and Restricted route components

--- src/components/RestrictedRoute.jsx
+++ src/components/RestrictedRoute.jsx
@@ -1,1 +1,19 @@
-// Restricted route will be added after authorization implementation
+import { useSelector } from 'react-redux';
+import { selectToken } from 'redux/auth/selectors';
+import { Navigate } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { ROUTER } from '../utils/constants';
+
+export const RestrictedRoute = ({
+  component: Component,
+  redirectTo = ROUTER.MAIN,
+}) => {
+  const token = useSelector(selectToken);
+
+  return token ? <Navigate to={redirectTo} /> : Component;
+};
+
+RestrictedRoute.propTypes = {
+  component: PropTypes.object.isRequired,
+  redirectTo: PropTypes.string,
+};
