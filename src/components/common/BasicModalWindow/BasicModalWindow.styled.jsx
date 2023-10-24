0bbd65ef5fe954804c8043743a83f4561700640d
EvgenBatt <evgenmgid@gmail.com>
2023-10-24 16:33:32 +0300

    Update modal window
    Додав заборону скролу при відкритій модалці. Видалив падінги з контейнеру модалки та спозиціонував абсолютом кнопку закриття модалки. В спрайті замінив іконку закриття модалки.

--- src/components/common/BasicModalWindow/BasicModalWindow.styled.jsx
+++ src/components/common/BasicModalWindow/BasicModalWindow.styled.jsx
@@ -1,23 +1,12 @@
-import { keyframes } from '@emotion/react';
 import styled from '@emotion/styled';
 
-const animationClose = keyframes`
-  from {
-    opacity: 1;
-  }
-  to {
-    opacity: 0;
-  }
-`;
-
 export const ModalBackdrop = styled.div`
-  background: rgba(0, 0, 0, 0.5);
+  background: rgba(0, 0, 0, 0.6);
   position: fixed;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
-  animation: ${animationClose} 300ms linear;
 `;
 
 export const Modal = styled.div`
@@ -28,17 +17,12 @@ export const Modal = styled.div`
   background-color: #10100f;
   border: 1px solid rgba(239, 237, 232, 0.2);
   border-radius: 12px;
-  padding: 14px;
   overflow-y: auto;
-
-  @media screen and (min-width: 768px) {
-    padding: 16px;
-  }
 `;
 
 export const CloseModalButton = styled.button`
-  width: 22px;
-  height: 22px;
+  width: 26px;
+  height: 26px;
   color: #efede8;
   background-color: transparent;
   border: none;
@@ -46,15 +30,20 @@ export const CloseModalButton = styled.button`
   align-items: center;
   justify-content: center;
   margin-left: auto;
+  position: absolute;
+  top: 14px;
+  right: 14px;
 
   @media screen and (min-width: 768px) {
-    width: 26px;
-    height: 26px;
+    top: 16px;
+    right: 16px;
+    width: 32px;
+    height: 32px;
   }
 `;
 
 export const CloseModalIcon = styled.svg`
   width: 100%;
   height: 100%;
-  fill: currentColor;
+  stroke: currentColor;
 `;
