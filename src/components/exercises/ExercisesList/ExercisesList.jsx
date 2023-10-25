f0894e8160827316873761d04298e7781d1d66fa
Sergey4857 <yasak.sergey@gmail.com>
2023-10-26 00:43:57 +0300

    +

--- src/components/exercises/ExercisesList/ExercisesList.jsx
+++ src/components/exercises/ExercisesList/ExercisesList.jsx
@@ -1,31 +1,67 @@
 import ExercisesItem from '../ExercisesItem/ExercisesItem';
-
 import { ExerciseList } from './ExercisesList.styled';
 import exercises from './exercises.json';
 import { useParams } from 'react-router';
-
-function bodyPartsFilter(value) {
-  return exercises
-    .filter(exercises => exercises.bodyPart === value)
-    .slice(0, 40);
-}
+import { useLazyFetchAllExercisesQuery } from '../../../redux/api';
+import { useEffect, useState } from 'react';
 
 export function ExercisesList() {
   const { subcategory } = useParams();
+  const [page, setPage] = useState(1);
+  const [exerciseData, setExerciseData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const [fetchAllExercises, { data }] = useLazyFetchAllExercisesQuery();
+
+  console.log(subcategory);
+  useEffect(() => {
+    const fetch = async () => {
+      try {
+        const response = await fetchAllExercises({ page }).unwrap();
+
+        const newData = response.map(item => ({
+          _id: item._id,
+          name: item.name,
+          bodyPart: item.bodyPart,
+          burnedCalories: item.burnedCalories,
+          target: item.target,
+        }));
+
+        setExerciseData(prevData => [...prevData, ...newData]);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    setLoading(true);
+    fetch();
+  }, [fetchAllExercises, page]);
 
   return (
-    <ExerciseList>
-      {bodyPartsFilter(subcategory).map(
-        ({ _id, name, bodyPart, burnedCalories, target }) => (
+    <div>
+      <ExerciseList>
+        {exerciseData.map(({ _id, name, bodyPart, burnedCalories, target }) => (
           <ExercisesItem
-            key={_id.$oid}
+            key={_id}
             name={name}
             bodyPart={bodyPart}
             burnedCalories={burnedCalories}
             target={target}
-          ></ExercisesItem>
-        ),
-      )}
-    </ExerciseList>
+          />
+        ))}
+      </ExerciseList>
+
+      <button
+        type="button"
+        onClick={() => {
+          setPage(page + 1);
+        }}
+      >
+        Add more
+      </button>
+    </div>
   );
 }
--- src/components/exercises/ExercisesSubcategoriesList/ExercisesSubcategoriesList.jsx
+++ src/components/exercises/ExercisesSubcategoriesList/ExercisesSubcategoriesList.jsx
@@ -1,5 +1,37 @@
-import { List } from '../ExercisesCategories/ExercisesSubcategoriesList.styled';
+import React, { useEffect } from 'react';
 
 export function ExercisesSubcategoriesList() {
-  return <div>Вмсекраплрол</div>;
+  return <div></div>;
 }
+// export function ExercisesList() {
+//   const { subcategory } = useParams();
+//   const [result, setResult] = useState();
+
+//   const [fetchAllExercises, { data }] = useLazyFetchAllExercisesQuery();
+//   useEffect(() => {
+//     const fetch = async () => {
+//       const subcategories = await fetchAllExercises().unwrap();
+//     };
+//     try {
+//       fetch();
+//       setResult(data);
+//     } catch (error) {
+//       console.log();
+//     }
+//   }, [fetchAllExercises]);
+
+//   console.log(result);
+//   return (
+//     <ExerciseList>
+//       {data.map(({ _id, name, bodyPart, burnedCalories, target }) => (
+//         <ExercisesItem
+//           key={_id.$oid}
+//           name={name}
+//           bodyPart={bodyPart}
+//           burnedCalories={burnedCalories}
+//           target={target}
+//         ></ExercisesItem>
+//       ))}
+//     </ExerciseList>
+//   );
+// }
