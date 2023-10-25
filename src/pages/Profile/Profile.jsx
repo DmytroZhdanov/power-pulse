66a79a536636795de5b81cb6fa6f87b2f72288a3
Dmytro Zhdanov <plumage-bassoon0u@icloud.com>
2023-10-25 16:18:18 +0300

    Update Profile.jsx

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -2,10 +2,9 @@ import TitlePage from 'components/common/TitlePage/TitlePage';
 import UserCard from 'components/profile/UserCard/UserCard';
 import UserForm from 'components/profile/UserForm/UserForm';
 
-export default function Profile() {
+export function Profile() {
   return (
     <>
-      {/* <div>Profile page</div> */}
       <TitlePage text="Profile Settings" />
       <UserCard />
       <UserForm />
