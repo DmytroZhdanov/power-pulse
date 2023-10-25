67ab6b0042165a4e7435a0f3b752b74cfb46c78f
RevaDaryna <revadara32@gmail.com>
2023-10-25 13:14:46 +0300

    Table

--- src/components/diary/DayProducts/DayProducts.jsx
+++ src/components/diary/DayProducts/DayProducts.jsx
@@ -1,10 +1,24 @@
 import ProductsTable from '../ProductsTable/ProductsTable';
 
+import sprite from '../../../assets/images/sprite/sprite.svg';
+
+import {
+  DayProdDiv,
+  DayProdTitle,
+  DayProdUl,
+  AddProdBtn,
+  AddProdBtnIcon,
+  AddProdBtnText
+} from './DayProducts.styled'
+
 export default function DayProducts() {
   return (
-    <>
-      <div>DayProducts component</div>
+    <DayProdDiv>
+      <DayProdUl>
+        <DayProdTitle>Products</DayProdTitle>
+        <AddProdBtn to='/Products'><AddProdBtnText>Add product</AddProdBtnText><AddProdBtnIcon><use href={`${sprite}#big_arrow`}></use></AddProdBtnIcon></AddProdBtn>
+      </DayProdUl>
       <ProductsTable />
-    </>
+    </DayProdDiv>
   );
 }
--- src/components/diary/ProductsTable/ProductsTable.jsx
+++ src/components/diary/ProductsTable/ProductsTable.jsx
@@ -1,3 +1,87 @@
+import {
+  Table,
+  TableTd,
+  TableTr,
+  TableTh,
+  TableDiv,
+  DelBtnTable,
+  DelIcon,
+  TableRecomSpan,
+  MobTitlesTr,
+  TableMainTitle,
+  TableMainTitles,
+  TableBody,
+  TableInfoTd
+} from './ProductsTable.styled'
+import sprite from '../../../assets/images/sprite/sprite.svg';
 export default function ProductsTable() {
-  return <div>ProductsTable component</div>;
+  return (
+    <>
+    <TableDiv>
+   <Table>
+    <TableMainTitles>
+      <TableMainTitle>Title</TableMainTitle>
+      <TableMainTitle>Category</TableMainTitle>
+      <TableMainTitle>Calories</TableMainTitle>
+      <TableMainTitle>Weight</TableMainTitle>
+      <TableMainTitle>Recommend</TableMainTitle>
+    </TableMainTitles>
+    <TableBody>
+      <TableTr>
+        <MobTitlesTr>
+          <TableTh>Title</TableTh>
+          <TableTh>Category</TableTh>
+          <TableTh>Calories</TableTh>
+          <TableTh>Weight</TableTh>
+          <TableTh>Recommend</TableTh>
+          {/* <TableTh>&nbsp;</TableTh> */}
+        </MobTitlesTr>
+        <TableInfoTd>
+        <TableTd>Bread Hercules grain</TableTd>
+        <TableTd>Cereals</TableTd>
+        <TableTd>289</TableTd>
+        <TableTd>100</TableTd>
+        <TableTd><TableRecomSpan Recom={true}/>Yes</TableTd>
+        <DelBtnTable><DelIcon> <use href={`${sprite}#delete`}></use></DelIcon></DelBtnTable>
+        </TableInfoTd>
+      </TableTr>
+      <TableTr>
+        <MobTitlesTr>
+          <TableTh>Title</TableTh>
+          <TableTh>Category</TableTh>
+          <TableTh>Calories</TableTh>
+          <TableTh>Weight</TableTh>
+          <TableTh>Recommend</TableTh>
+        </MobTitlesTr>
+        <TableInfoTd>
+        <TableTd>Bread Hercules grain</TableTd>
+        <TableTd>Cereals</TableTd>
+        <TableTd>289</TableTd>
+        <TableTd>100</TableTd>
+        <TableTd><TableRecomSpan Recom={true}/>Yes</TableTd>
+        <DelBtnTable><DelIcon> <use href={`${sprite}#delete`}></use></DelIcon></DelBtnTable>
+        </TableInfoTd>
+      </TableTr>
+      <TableTr>
+        <MobTitlesTr>
+          <TableTh>Title</TableTh>
+          <TableTh>Category</TableTh>
+          <TableTh>Calories</TableTh>
+          <TableTh>Weight</TableTh>
+          <TableTh>Recommend</TableTh>
+        </MobTitlesTr>
+        <TableInfoTd>
+        <TableTd>Bread Hercules grain</TableTd>
+        <TableTd>Cereals</TableTd>
+        <TableTd>289</TableTd>
+        <TableTd>100</TableTd>
+        <TableTd><TableRecomSpan Recom={true}/>Yes</TableTd>
+        <DelBtnTable><DelIcon> <use href={`${sprite}#delete`}></use></DelIcon></DelBtnTable>
+        </TableInfoTd>
+      </TableTr>
+    </TableBody>
+   </Table>
+   </TableDiv>
+    </>
+  )
 }
