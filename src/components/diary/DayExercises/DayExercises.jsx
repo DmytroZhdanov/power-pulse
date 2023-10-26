76ddfadf5887a4cb7ff0fa16faa0224ca64058aa
RevaDaryna <revadara32@gmail.com>
2023-10-26 19:20:08 +0300

    add exercises section

--- src/components/diary/DayExercises/DayExercises.jsx
+++ src/components/diary/DayExercises/DayExercises.jsx
@@ -1,10 +1,23 @@
 import ExercisesTable from '../ExercisesTable/ExercisesTable';
+import sprite from '../../../assets/images/sprite/sprite.svg';
+
+import {
+  DayProdDiv,
+  DayProdTitle,
+  DayProdUl,
+  AddProdBtn,
+  AddProdBtnIcon,
+  AddProdBtnText
+} from './DayExercises.styled'
 
 export default function DayExercises() {
   return (
-    <>
-      <div>DayExercises component</div>
+    <DayProdDiv>
+      <DayProdUl>
+        <DayProdTitle>Exercises</DayProdTitle>
+        <AddProdBtn to='/exercises'><AddProdBtnText>Add exercise</AddProdBtnText><AddProdBtnIcon><use href={`${sprite}#big_arrow`}></use></AddProdBtnIcon></AddProdBtn>
+      </DayProdUl>
       <ExercisesTable />
-    </>
+    </DayProdDiv>
   );
 }
---
+++ src/components/diary/ExercisesTable/ExercisesTable.styled.js
@@ -0,0 +1,224 @@
+import styled from '@emotion/styled';
+
+export const TableDiv = styled.div`
+  @media screen and (max-width: 767px) {
+    max-height: 1000px;
+    max-width: 335px;
+  }
+  @media screen and (min-width: 768px) {
+    max-height: 178px;
+    max-width: 704px;
+  }
+  @media screen and (min-width: 1440px) {
+    max-width: 804px;
+    padding: 0 8px
+  }
+  scroll-behavior: auto;
+  // overflow-y: auto;
+  overflow-y: hidden;
+  overflow-x: hidden;
+`;
+export const Table = styled.table`
+border-spacing: 8px ;
+border-collapse: separate;
+@media screen and (max-width: 767px) {
+  position: relative;
+    height: 342px;
+    width: 100%;
+    &:last-child {
+      height: 312px;
+    }
+  }
+`;
+
+export const TableMainTitles = styled.thead`
+text-align: left;
+`;
+
+export const TableTitleTr = styled.tr`
+font-size: 12px;
+  line-height: ${18 / 12};
+  color: #ef8964;
+`;
+
+export const TableMainTitle = styled.th`
+@media screen and (max-width: 767px) {
+  position: absolute; 
+  &:nth-of-type(2) {
+    top: 88px;
+  }
+  &:nth-of-type(3) {
+    top: 168px;
+  }
+  &:nth-of-type(4) {
+    left: 8px;
+    top: 248px;
+  }
+  &:nth-of-type(5) {
+    top: 248px;
+    left: 105px;
+  }
+  &:nth-of-type(6) {
+    top: 248px;
+    left: 201px;
+  }
+}
+
+@media screen and (min-width: 768px) {
+  &:first-of-type {
+    width: 90px;
+  }
+  &:nth-of-type(2) {
+    width: 132px;
+  }
+  &:nth-of-type(3) {
+    width: 128px;
+  }
+  &:nth-of-type(4) {
+    width: 84px;
+  }
+  &:nth-of-type(5) {
+    width: 78px;
+  }
+  &:nth-of-type(6) {
+    width: 72px;
+  }
+}
+
+@media screen and (min-width: 1440px) {
+  &:first-of-type {
+    width: 115px;
+  }
+  &:nth-of-type(2) {
+    width: 157px;
+  }
+  &:nth-of-type(3) {
+    width: 131px;
+  }
+  &:nth-of-type(4) {
+    width: 106px;
+  }
+  &:nth-of-type(5) {
+    width: 91px;
+  }
+  &:nth-of-type(6) {
+    width: 82px;
+  }
+`;
+
+export const TableTr = styled.tr`
+ 
+`;
+export const DelBtnTable = styled.button`
+  width: 20px;
+  height: 20px;
+  background: transparent;
+  border: none;
+  background-color: transparent;
+  padding: 0;
+  @media screen and (max-width: 767px) {
+    position: absolute;
+    top: 283px;
+    left: 285px;
+  }
+  @media screen and (min-width: 768px) {
+    padding-left:  4px
+  }
+  @media screen and (min-width: 1440px) {
+    padding-left:  12px
+
+  }
+  `;
+export const DelIcon = styled.svg`
+  width: 20px;
+  height: 20px;
+`;
+
+export const TableBody = styled.tbody`
+
+`
+export const TableInfoTd = styled.td`
+border-radius: 12px;
+color: #efede8;
+border: 1px solid rgba(239, 237, 232, 0.3);
+@media screen and (max-width: 767px) {
+  position: absolute; 
+  padding: 10px 14px;
+  font-size: 14px;
+  line-height: ${18 / 14};
+  &:first-of-type {
+    top: 34px;
+    width: 297px;
+  }
+  &:nth-of-type(2) {
+    top: 114px;
+    width: 297px;
+  }
+  &:nth-of-type(3) {
+    top: 194px;
+    width: 297px;
+  }
+  &:nth-of-type(4) {
+    top: 274px;
+    left: 8px;
+    width: 81px;
+  }
+  &:nth-of-type(5) {
+    top: 274px;
+    left: 105px;
+    width: 80px;
+  }
+  &:nth-of-type(6) {
+    top: 274px;
+    left: 201px;
+    width: 76px;
+  }
+}
+
+@media screen and (min-width: 768px) {
+  position: relative;
+  font-size: 16px;
+  line-height: ${24 / 16};
+  padding: 8px 14px;
+  &:first-of-type {
+    width: 90px;
+  }
+  &:nth-of-type(2) {
+    width: 132px;
+  }
+  &:nth-of-type(3) {
+    width: 128px;
+  }
+  &:nth-of-type(4) {
+    width: 84px;
+  }
+  &:nth-of-type(5) {
+    width: 78px;
+  }
+  &:nth-of-type(6) {
+    width: 72px;
+  }
+}
+@media screen and (min-width: 1440px) {
+  &:first-of-type {
+    width: 115px;
+  }
+  &:nth-of-type(2) {
+    width: 157px;
+  }
+  &:nth-of-type(3) {
+    width: 131px;
+  }
+  &:nth-of-type(4) {
+    width: 106px;
+  }
+  &:nth-of-type(5) {
+    width: 91px;
+  }
+  &:nth-of-type(6) {
+    width: 82px;
+  }
+  }
+}
+
+`
