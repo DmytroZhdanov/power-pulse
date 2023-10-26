c83bda5211f26e35ddef7f07716ebaaef28f2c6b
Sergey4857 <yasak.sergey@gmail.com>
2023-10-26 19:35:50 +0300

    +

--- src/components/exercises/ExercisesList/ExercisesList.jsx
+++ src/components/exercises/ExercisesList/ExercisesList.jsx
@@ -15,12 +15,15 @@ export function ExercisesList() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
+  const equipment = 'barbell';
+  // const bodyPart = 'upper legs';
+
   const [fetchAllExercises, { data }] = useLazyFetchAllExercisesQuery();
 
   useEffect(() => {
     const fetch = async () => {
       try {
-        await fetchAllExercises({ page }).unwrap();
+        await fetchAllExercises({ page, equipment }).unwrap();
       } catch (error) {
         setError(error);
       } finally {
@@ -30,7 +33,9 @@ export function ExercisesList() {
 
     setLoading(true);
     fetch();
-  }, [fetchAllExercises, page]);
+  }, [fetchAllExercises, page, equipment]);
+
+  console.log(data);
 
   const location = useLocation();
   const pathLocation = useRef(location.state?.from ?? '/exercises');
@@ -67,3 +72,78 @@ export function ExercisesList() {
     </>
   );
 }
+
+// import ExercisesItem from '../ExercisesItem/ExercisesItem';
+// import {
+//   ExerciseList,
+//   LinkText,
+//   StyledLink,
+//   Svg,
+// } from './ExercisesList.styled';
+// import { useLocation, useParams } from 'react-router';
+// import { useLazyFetchAllExercisesQuery } from '../../../redux/api';
+// import { useEffect, useRef, useState } from 'react';
+// import sprite from 'src/assets/images/sprite/sprite.svg';
+
+// export function ExercisesList() {
+//   const [page, setPage] = useState(1);
+//   const [loading, setLoading] = useState(false);
+//   const [error, setError] = useState(null);
+
+//   const equipment = 'barbell';
+
+//   const [fetchAllExercises, { data }] = useLazyFetchAllExercisesQuery();
+
+//   useEffect(() => {
+//     const fetch = async () => {
+//       try {
+//         // Добавьте параметр equipment к запросу
+//         await fetchAllExercises({ page, equipment }).unwrap();
+//       } catch (error) {
+//         setError(error);
+//       } finally {
+//         setLoading(false);
+//       }
+//     };
+
+//     setLoading(true);
+//     fetch();
+//   }, [fetchAllExercises, page, equipment]);
+
+//   console.log(data);
+
+//   const location = useLocation();
+//   const pathLocation = useRef(location.state?.from ?? '/exercises');
+
+//   return (
+//     <>
+//       <StyledLink to={pathLocation.current}>
+//         <Svg>
+//           <use href={`${sprite}#icon-arrow`}></use>
+//         </Svg>
+//         <LinkText>Back</LinkText>
+//       </StyledLink>
+
+//       <ExerciseList>
+//         {data?.map(({ _id, name, bodyPart, burnedCalories, target }) => (
+//           <ExercisesItem
+//             key={_id}
+//             name={name}
+//             bodyPart={bodyPart}
+//             burnedCalories={burnedCalories}
+//             target={target}
+//           />
+//         ))}
+//       </ExerciseList>
+
+//       <button
+//         type="button"
+//         onClick={() => {
+//           setPage(page + 1);
+//         }}
+//       >
+//         Add more
+//       </button>
+//     </>
+//   );
+// }
