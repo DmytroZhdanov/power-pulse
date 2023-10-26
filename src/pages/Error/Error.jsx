5471ae5948a3818a25834822e3b905e2b763f648
EvgenBatt <evgenmgid@gmail.com>
2023-10-26 23:35:10 +0300

    Add Error Page
    Зробив сторінку 404. Стилі на всі розміри екрану. Є деякі моменти, які на міті треба обговорити з приводу правої картинки. І ще в Хедері треба стилі трохи змінити.

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,8 +1,44 @@
-import { useRouteError } from 'react-router-dom';
+import { useLocation, useRouteError } from 'react-router-dom';
+import { useRef } from 'react';
+import { ROUTER } from 'src/utils/constants';
+import Header from 'components/main/Header/Header';
+import {
+  BackgroundImage,
+  BackgroundImageWrap,
+  ContentWrap,
+  Flex,
+  LinkStyled,
+  Text,
+  Title,
+} from './Error.styled';
 
 export default function Error() {
   const error = useRouteError();
-  console.log(error)
+  console.log(error);
+  const location = useLocation();
 
-  return <div>Error Page if no any required route found</div>
+  const goBackLink = useRef(location?.state?.from ?? ROUTER.MAIN);
+
+  return (
+    <>
+      <Header />
+      <main>
+        <Flex>
+          <ContentWrap>
+            <Title>404</Title>
+            <Text>
+              Sorry, you have reached a page that we could not find. It seems
+              that you are lost among the numbers and letters of our virtual
+              space. Perhaps this page went on vacation or decided to disappear
+              into another dimension. We apologize for this inconvenience.
+            </Text>
+            <LinkStyled to={goBackLink.current}>Go Home</LinkStyled>
+          </ContentWrap>
+          <BackgroundImageWrap>
+            <BackgroundImage />
+          </BackgroundImageWrap>
+        </Flex>
+      </main>
+    </>
+  );
 }
