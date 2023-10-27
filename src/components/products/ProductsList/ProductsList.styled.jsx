1028188bebbe7a5e3744c61b21949a56b6329c33
Rinya7 <v.dmitriev0712@gmail.com>
2023-10-27 16:23:26 +0200

    default text

--- src/components/products/ProductsList/ProductsList.styled.jsx
+++ src/components/products/ProductsList/ProductsList.styled.jsx
@@ -22,3 +22,32 @@ export const ProductList = styled.ul`
     width: 850px;
   }
 `;
+
+export const DefaultText = styled.p`
+  margin-bottom: 16px;
+  color: rgba(239, 237, 232, 0.3);
+  font-size: 14px;
+  line-height: calc(18 / 14);
+
+  @media (min-width: 768px) {
+    width: 580px;
+    font-size: 16px;
+    line-height: calc(24 / 16);
+  }
+`;
+
+export const SpanNotFound = styled.span`
+  color: #e6533c;
+`;
+
+export const SpanTry = styled.span`
+  margin-top: 16px;
+  color: #e6533c;
+  font-size: 14px;
+  line-height: calc(18 / 14);
+
+  @media (min-width: 768px) {
+    font-size: 16px;
+    line-height: calc(24 / 16);
+  }
+`;
