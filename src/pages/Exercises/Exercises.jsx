fd8044998a552a4796e90b4d80fb8523ba8029c6
Sergey4857 <yasak.sergey@gmail.com>
2023-10-27 16:27:07 +0300

    refactor

--- src/pages/Exercises/Exercises.jsx
+++ src/pages/Exercises/Exercises.jsx
@@ -1,17 +1,13 @@
 import TitlePage from 'components/common/TitlePage/TitlePage';
 import { EXERCISES_CATEGORY } from '../../utils/constants';
 import ExercisesCategories from 'components/exercises/ExercisesCategories/ExercisesCategories';
-import { Outlet, useLocation, useParams } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { useState } from 'react';
-import { ExercisesSubcategoriesList } from '../../components/exercises/ExercisesSubcategoriesList/ExercisesSubcategoriesList';
+
 import { Wrapper } from './Exercises.styled';
-import UpperCase from '../../components/common/UpperCaseFunc/UpperCase';
 
 export function Exercises() {
   const [category, setCategory] = useState(EXERCISES_CATEGORY.BODY_PARTS);
-  const location = useLocation();
-  const { subcategory } = useParams();
-  const exercisesRoute = location.pathname === '/exercises';
 
   return (
     <>
