6c1a77035e5020de14ca33f5ceec0729c9fce743
DmytroZhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2023-10-27 18:48:06 +0300

    Update dependences to useEffect in ProductsFilters

--- src/components/products/ProductsFilters/ProductsFilters.jsx
+++ src/components/products/ProductsFilters/ProductsFilters.jsx
@@ -37,7 +37,7 @@ export default function ProductsFilters({ onProductsChange }) {
 
   useEffect(() => {
     onProductsChange(filter);
-  }, [filter]);
+  }, [filter, onProductsChange]);
 
   const handleClean = () => {
     setSearch('');
