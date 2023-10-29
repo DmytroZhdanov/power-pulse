7c6912edb9327ba5e9d527b118019f2888db3403
Sergey4857 <yasak.sergey@gmail.com>
2023-10-29 12:43:22 +0200

    fx

--- src/components/common/UpperCaseFunc/UpperCase.js
+++ src/components/common/UpperCaseFunc/UpperCase.js
@@ -1,3 +1,8 @@
+/**
+ * МІняє першу букву в рядку на UpperCase
+ * @param {String} s
+ * @returns String
+ */
 const UpperCase = s => (s && s[0].toUpperCase() + s.slice(1)) || '';
 
 export default UpperCase;
