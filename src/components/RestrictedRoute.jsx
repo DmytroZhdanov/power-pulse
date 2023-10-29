3bab7e927c0d1508fba72ff812e1c435d4bac995
DmytroZhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2023-10-29 19:21:50 +0200

    Update Private and Restricted routes

--- src/components/RestrictedRoute.jsx
+++ src/components/RestrictedRoute.jsx
@@ -6,10 +6,10 @@ import { useFetchUserParamsQuery } from 'src/redux/api';
 import { ROUTER } from 'src/utils';
 
 export default function RestrictedRoute({ component: Component, redirectTo }) {
-  const { data, isError } = useFetchUserParamsQuery();
+  const { data, isFetching, isError } = useFetchUserParamsQuery();
   const token = useSelector(selectToken);
 
-  if ((!data?.user.userParams || isError) && token) {
+  if (((!data?.user.userParams && isFetching) || isError) && token) {
     return <Navigate to={`../${ROUTER.DATA}`} />;
   }
 
