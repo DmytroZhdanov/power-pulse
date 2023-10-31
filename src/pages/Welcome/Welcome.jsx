61e2898015d93df5dfaba3c5788576ec68972f21
DmytroZhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2023-10-31 08:09:01 +0200

    Refactor and add comments to Welcome

--- src/pages/Welcome/Welcome.jsx
+++ src/pages/Welcome/Welcome.jsx
@@ -1,10 +1,8 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { useLocation } from 'react-router-dom';
-import { ROUTER } from 'src/utils/constants';
-import ErrorHandler from '../../components/common/ErrorHandler/ErrorHandler';
-import { useLazyRefreshQuery } from '../../redux/api';
-import { setCredentials, setToken } from '../../redux/auth/authSlice';
+
+import ErrorHandler from 'components/common/ErrorHandler/ErrorHandler';
 import {
   Button,
   ButtonWrapper,
@@ -13,13 +11,18 @@ import {
   Title,
 } from './Welcome.styled';
 
+import { useLazyRefreshQuery } from 'src/redux/api';
+import { setCredentials, setToken } from 'src/redux/auth/authSlice';
+import { ROUTER } from 'src/utils/constants';
+
 export function Welcome() {
   const dispatch = useDispatch();
   const [refresh, { isFetching, isError, error }] = useLazyRefreshQuery();
   const params = useLocation();
 
-  const token = params.search.split('token=')[1]?.replace('#', '');
+  const token = params.search.split('token=')[1]?.replace('#', ''); // Reads token from url in case of google authorization
 
+  // If there is a token in url, set it to redux store
   useEffect(() => {
     const refetch = async () => {
       if (token) {
