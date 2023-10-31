a6853e6933ae3e004dbb462f85407b17ca135e05
DmytroZhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2023-10-31 09:00:47 +0200

    Refactor StatisticsInfo, add comments

--- src/components/RestrictedRoute.jsx
+++ src/components/RestrictedRoute.jsx
@@ -1,6 +1,7 @@
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
+
 import { selectToken } from 'src/redux/auth/selectors';
 
 export default function RestrictedRoute({ component: Component, redirectTo }) {
