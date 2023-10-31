3e5d5ecb6590b208f65c519ea36173e14da92c73
DmytroZhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2023-10-31 09:47:38 +0200

    Refactor Logo

--- src/components/main/Logo/Logo.jsx
+++ src/components/main/Logo/Logo.jsx
@@ -1,12 +1,14 @@
-import { LogoLink, SvgIcon, Title } from './Logo.styles';
-import sprite from '../../../assets/images/sprite/sprite.svg';
+import { LogoLink, SvgIcon, TitleP } from './Logo.styles';
+import sprite from 'src/assets/images/sprite/sprite.svg';
+
 export default function Logo() {
   return (
     <LogoLink to="/">
       <SvgIcon>
         <use href={`${sprite}#logo`}></use>
       </SvgIcon>
-      <Title>PowerPulse</Title>
+
+      <TitleP>PowerPulse</TitleP>
     </LogoLink>
   );
 }
