88612fdd4e8213abd46114a508406c78ece87861
Sergey4857 <yasak.sergey@gmail.com>
2023-11-02 16:13:29 +0200

    +

--- src/pages/Products/Products.styled.js
+++ src/pages/Products/Products.styled.js
@@ -3,33 +3,47 @@ import {
   bg_eating,
   bg_eating2x,
   bg_eating3x,
-  bg_hours,
-  mo_main,
-  mo_ex,
   Alcoholicdrinks,
   Berries,
+  Cereals,
+  Dairy,
+  Driedfruits,
+  Eggs,
+  Fish,
+  Flour,
+  Fruits,
+  Meat,
+  Mushrooms,
+  Nuts,
+  Oilsandfats,
+  Poppy,
+  Sausage,
+  Seeds,
+  Sesame,
+  Softdrinks,
+  Vegetablesandherbs,
 } from 'src/utils/background';
 
 const categoryImages = {
   Alcoholicdrinks: `url(${Alcoholicdrinks})`,
   Berries: `url(${Berries})`,
-  Cereals: `url(${bg_eating})`,
-  Dairy: `url(${mo_ex})`,
-  Driedfruits: `url(${mo_ex})`,
-  Eggs: `url(${mo_ex})`,
-  Fish: `url(${mo_ex})`,
-  Flour: `url(${mo_ex})`,
-  Fruits: `url(${mo_ex})`,
-  Meat: `url(${mo_ex})`,
-  Mushrooms: `url(${mo_ex})`,
-  Nuts: `url(${mo_ex})`,
-  Oilsandfats: `url(${mo_ex})`,
-  Poppy: `url(${mo_ex})`,
-  Sausage: `url(${mo_ex})`,
-  Seeds: `url(${mo_ex})`,
-  Sesame: `url(${mo_ex})`,
-  Softdrinks: `url(${mo_ex})`,
-  Vegetablesandherbs: `url(${mo_ex})`,
+  Cereals: `url(${Cereals})`,
+  Dairy: `url(${Dairy})`,
+  Driedfruits: `url(${Driedfruits})`,
+  Eggs: `url(${Eggs})`,
+  Fish: `url(${Fish})`,
+  Flour: `url(${Flour})`,
+  Fruits: `url(${Fruits})`,
+  Meat: `url(${Meat})`,
+  Mushrooms: `url(${Mushrooms})`,
+  Nuts: `url(${Nuts})`,
+  Oilsandfats: `url(${Oilsandfats})`,
+  Poppy: `url(${Poppy})`,
+  Sausage: `url(${Sausage})`,
+  Seeds: `url(${Seeds})`,
+  Sesame: `url(${Sesame})`,
+  Softdrinks: `url(${Softdrinks})`,
+  Vegetablesandherbs: `url(${Vegetablesandherbs})`,
 };
 
 export const BackgroundDiv = styled.div`
@@ -38,14 +52,18 @@ export const BackgroundDiv = styled.div`
   right: -76px;
   width: 428px;
   height: 716px;
-
-  background-image: ${props =>
-    categoryImages[props?.filter] ||
-    `image-set(
+  border-top-right-radius: 12px;
+  border-bottom-right-radius: 12px;
+  background:
+    linear-gradient(90deg, #040404 1.1%, rgba(4, 4, 4, 0) 70.79%),
+    linear-gradient(180deg, #040404 1.1%, rgba(4, 4, 4, 0) 50.79%),
+    ${props =>
+      categoryImages[props?.filter] ||
+      `image-set(
     url(${bg_eating}) 1x,
     url(${bg_eating2x}) 2x,
-      url(${bg_eating3x}) 3x)`};
-
+      url(${bg_eating3x}) 3x)`},
+    lightgray -372.891px -2.705px / 133.568% 100.375% no-repeat;
   background-size: 428px 716px;
 `;
 
--- src/utils/background.js
+++ src/utils/background.js
@@ -37,7 +37,7 @@ import bg_main3x from 'src/assets/images/background/Desktop/bg_main@3x.jpg';
 import bg_users from 'src/assets/images/background/Desktop/bg_users.jpg';
 import bg_users2x from 'src/assets/images/background/Desktop/bg_users@2x.jpg';
 import bg_users3x from 'src/assets/images/background/Desktop/bg_users@3x.jpg';
-
+Mushrooms;
 import bg_hours from 'src/assets/images/background/Desktop/bg_hours.jpg';
 import bg_hours2x from 'src/assets/images/background/Desktop/bg_hours@2x.jpg';
 import bg_hours3x from 'src/assets/images/background/Desktop/bg_hours@3x.jpg';
@@ -52,6 +52,23 @@ import bg_eating3x from 'src/assets/images/background/Desktop/bg_eating@3x.jpg';
 
 import Alcoholicdrinks from '../assets/images/productsBackground/Alcoholicdrinks.jpg';
 import Berries from '../assets/images/productsBackground/Berries.jpg';
+import Cereals from '../assets/images/productsBackground/Cereals.jpg';
+import Dairy from '../assets/images/productsBackground/Dairy.jpg';
+import Driedfruits from '../assets/images/productsBackground/Driedfruits.jpg';
+import Eggs from '../assets/images/productsBackground/Eggs.jpg';
+import Fish from '../assets/images/productsBackground/Fish.jpg';
+import Flour from '../assets/images/productsBackground/Flour.jpg';
+import Fruits from '../assets/images/productsBackground/Fruits.jpg';
+import Meat from '../assets/images/productsBackground/Meat.jpg';
+import Mushrooms from '../assets/images/productsBackground/Mushrooms.jpg';
+import Nuts from '../assets/images/productsBackground/Nuts.jpg';
+import Oilsandfats from '../assets/images/productsBackground/Oilsandfats.jpg';
+import Poppy from '../assets/images/productsBackground/Poppy.jpg';
+import Sausage from '../assets/images/productsBackground/Sausage.jpg';
+import Seeds from '../assets/images/productsBackground/Seeds.jpg';
+import Sesame from '../assets/images/productsBackground/Sesame.jpg';
+import Softdrinks from '../assets/images/productsBackground/Softdrinks.jpg';
+import Vegetablesandherbs from '../assets/images/productsBackground/Vegetablesandherbs.jpg';
 
 export {
   mo_main,
@@ -95,4 +112,21 @@ export {
   bg_eating3x,
   Alcoholicdrinks,
   Berries,
+  Cereals,
+  Dairy,
+  Driedfruits,
+  Eggs,
+  Fish,
+  Flour,
+  Fruits,
+  Meat,
+  Mushrooms,
+  Nuts,
+  Oilsandfats,
+  Poppy,
+  Sausage,
+  Seeds,
+  Sesame,
+  Softdrinks,
+  Vegetablesandherbs,
 };
