310aa2cdf432e13c3fec5ed5fe9a6cfea684ae7e
vasylkivt <vasylkiv.t@gmail.com>
2023-11-05 21:00:15 +0200

    fix error

--- src/i18n.js
+++ src/i18n.js
@@ -1,12 +1,12 @@
 import i18n from 'i18next';
-import I18nextBrowserLanguageDetector from 'i18next-browser-languagedetector';
+// import I18nextBrowserLanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-http-backend';
 
 import { initReactI18next } from 'react-i18next';
 
 i18n
   .use(Backend)
-  .use(I18nextBrowserLanguageDetector)
+  // .use(I18nextBrowserLanguageDetector)
   .use(initReactI18next)
   .init({
     fallbackLng: 'en',
