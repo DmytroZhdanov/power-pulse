b2a13999e7fb3fc4f27f3669428689c11df9b038
vasylkivt <vasylkiv.t@gmail.com>
2023-11-22 12:36:18 +0200

    add "ns: ['common']" as a default translation file;

--- src/i18n.js
+++ src/i18n.js
@@ -1,16 +1,13 @@
 import i18n from 'i18next';
-// import I18nextBrowserLanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-http-backend';
-
 import { initReactI18next } from 'react-i18next';
 
 i18n
   .use(Backend)
-  // .use(I18nextBrowserLanguageDetector)
   .use(initReactI18next)
   .init({
+    ns: ['common'],
     fallbackLng: 'en',
-    // debug: true,
   });
 
 export default i18n;
